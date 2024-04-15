a9aac4f6eb89d179fb1753e31ca937e22b0a4380
Konrad Lorenz <86967062+K-Lorenz@users.noreply.github.com>
2024-04-15 19:04:42 +0200

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-azure-swa';
 import sveltePreprocess from 'svelte-preprocess'; // Corrected import
 
 /** @type {import('@sveltejs/kit').Config} */
